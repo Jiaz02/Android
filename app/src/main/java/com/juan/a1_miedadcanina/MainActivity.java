c711f2073d9aec70dd32e29b9fd5d6fc25919ce9
Jiaz02 <jsaavedraa@iesch.org>
2021-09-24 14:15:45 +0200

    jaja bromita

--- app/src/main/java/com/juan/a1_miedadcanina/MainActivity.java
+++ app/src/main/java/com/juan/a1_miedadcanina/MainActivity.java
@@ -3,6 +3,7 @@ package com.juan.a1_miedadcanina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,19 +20,30 @@ public class MainActivity extends AppCompatActivity {
         TextView resultadoText = findViewById(R.id.resultado_et);
         Button boton = findViewById(R.id.button_btn);
 
+        Log.v("MainActivity", "La actividad principal se ha creado");
+        Log.d("MainActivity", "La actividad principal se ha creado");
+        Log.i("MainActivity", "La actividad principal se ha creado");
+        Log.w("MainActivity", "La actividad principal se ha creado");
+        Log.e("MainActivity", "La actividad principal se ha creado");
+
         //2- Damos actividad al boton al ser pulsado
         boton.setOnClickListener( view -> {
             //3- recogemos el texto del edit text
             String edad = edadEdit.getText().toString();
-            //4- convertimos el string a entero
-            int edadInt =Integer.parseInt(edad);
-            //5- Calculamos la edad canina
-            int resultado = edadInt * 7;
-            //6- creamos el texto que vamos a mostrar
-            String resultadoString = "Si fueras un perro tu edad sería "+resultado+" años";
-            //7- asignamos el texto al elemento textview
-            resultadoText.setText(resultadoString);
 
+            if (!edad.isEmpty()) {
+
+                //4- convertimos el string a entero
+                int edadInt = Integer.parseInt(edad);
+                //5- Calculamos la edad canina
+                int resultado = edadInt * 7;
+                //6- creamos el texto que vamos a mostrar
+                String resultadoString = "Si fueras un perro tu edad sería " + resultado + " años";
+                //7- asignamos el texto al elemento textview
+                resultadoText.setText(resultadoString);
+            } else{
+                Log.d("MainActivity","Has introducido un campo vacio");
+            }
         });
     }
 }
