70f2fa8e6cd062ba55fd9de447b5dd0d24d5ffa0
Jiaz02 <jsaavedraa@iesch.org>
2021-09-28 16:29:19 +0200

    cosas

--- 2CicloDeVida/app/src/main/java/com/juan/a2_ciclodevida/MainActivity.kt
+++ 2CicloDeVida/app/src/main/java/com/juan/a2_ciclodevida/MainActivity.kt
@@ -2,10 +2,39 @@ package com.juan.a2_ciclodevida
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("CICLODEVIDA_ANDROID","Entramos al metodo onCreate() ")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("CICLODEVIDA_ANDROID","Entramos al metodo onStart() ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("CICLODEVIDA_ANDROID","Entramos al metodo onReturn() ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("CICLODEVIDA_ANDROID","Entramos al metodo onPause() ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("CICLODEVIDA_ANDROID","Entramos al metodo onStop() ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("CICLODEVIDA_ANDROID","Entramos al metodo onDestroy() ")
+    }
+
 }
