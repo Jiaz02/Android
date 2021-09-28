e6bb3746e9b592947dfb08033ccd769cef3d8374
Jiaz02 <jsaavedraa@iesch.org>
2021-09-28 17:21:21 +0200

    creacion ir de una pantalla a otra

--- 3VariosActivities/app/src/main/java/com/juan/a3_variosactivities/MainActivity.java
+++ 3VariosActivities/app/src/main/java/com/juan/a3_variosactivities/MainActivity.java
@@ -2,13 +2,33 @@ package com.juan.a3_variosactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnIrAc2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIrAc2 = findViewById(R.id.btn1);
+
+        btnIrAc2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        //Para cambiar de activity lo primero que haremos sera crear el Inten
+        //1--le diremos en el primer campo desde dodne le llamamos y en el segundo a donde queremos ir
+        Intent irAACtivity2 = new Intent(MainActivity.this,Segundaactivity.class);
+
+        //2--Lanzamos el activitycon el metodo startActivity
+        startActivity(irAACtivity2);
     }
 }
--- 3VariosActivities/app/src/main/java/com/juan/a3_variosactivities/Segundaactivity.java
+++ 3VariosActivities/app/src/main/java/com/juan/a3_variosactivities/Segundaactivity.java
@@ -2,13 +2,33 @@ package com.juan.a3_variosactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Segundaactivity extends AppCompatActivity {
+public class Segundaactivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnIrAc1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segundaactivity);
+
+        btnIrAc1 = findViewById(R.id.btn2);
+
+        btnIrAc1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        //Para cambiar de activity lo primero que haremos sera crear el Intent
+        //1--le diremos en el primer campo desde dodne le llamamos y en el segundo a donde queremos ir
+        Intent irAACtivity1 = new Intent(Segundaactivity.
+                this,MainActivity.class);
+
+        //2--Lanzamos el activitycon el metodo startActivity
+        startActivity(irAACtivity1);
     }
 }
