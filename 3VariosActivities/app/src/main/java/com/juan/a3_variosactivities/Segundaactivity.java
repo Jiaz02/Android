2571c3487383b7e9a874c53b92c77c73841fae9d
Jiaz02 <jsaavedraa@iesch.org>
2021-09-28 17:01:12 +0200

    creacion segunda activity

---
+++ 3VariosActivities/app/src/main/java/com/juan/a3_variosactivities/Segundaactivity.java
@@ -0,0 +1,14 @@
+package com.juan.a3_variosactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Segundaactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segundaactivity);
+    }
+}
