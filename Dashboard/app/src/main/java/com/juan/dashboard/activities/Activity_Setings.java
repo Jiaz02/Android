024395034d4d3bab3643b8d1c927b02b58ae48e7
Jiaz02 <jsaavedraa@iesch.org>
2021-10-19 17:55:17 +0200

    ya ni idea de que pasa

---
+++ Dashboard/app/src/main/java/com/juan/dashboard/activities/Activity_Setings.java
@@ -0,0 +1,16 @@
+package com.juan.dashboard.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.juan.dashboard.R;
+
+public class Activity_Setings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setings);
+    }
+}
