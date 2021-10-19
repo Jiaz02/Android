e9c860b39b57619c9c6a7fbd09b63b5b6613d38e
Jiaz02 <jsaavedraa@iesch.org>
2021-10-19 23:02:44 +0200

    poco

--- Quizees/app/src/main/java/com/juan/quizees/MainActivity.java
+++ Quizees/app/src/main/java/com/juan/quizees/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -17,15 +18,16 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public ArrayList<Pregunta> preguntas = new ArrayList<Pregunta>();
-
+    private int respuestasCorrectas = 0;
+    private int respuestasIncorrectas = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SystemClock.sleep(4000);
-        int respuestasCorrectas = 0;
-        int respuestasIncorrectas = 0;
+
+
 
         TextView pregunta = findViewById(R.id.idPregunta);
 
@@ -34,10 +36,85 @@ public class MainActivity extends AppCompatActivity {
         RadioButton rp2 = findViewById(R.id.radioButtonRespuesta2);
         RadioButton rp3 = findViewById(R.id.radioButtonRespuesta3);
 
+        Button btnSend = findViewById(R.id.buttonEnviar);
+
+
+        btnSend.setOnClickListener(view -> {
+
+            String Seleccion="";
+
+            String aux1=getResources().getString(R.string.Respuesta1);
+            String aux2=getResources().getString(R.string.Respuesta2);
+            String aux3=getResources().getString(R.string.Respuesta3);
+            String aux4=getResources().getString(R.string.Pregunta);
+            String[] respuestasp1 = {aux1,aux2,aux3};
+
+            Pregunta p1= new Pregunta(1,aux4,respuestasp1,aux2);
+            preguntas.add(p1);
+            aux1=getResources().getString(R.string.Respuesta1);
+            aux2=getResources().getString(R.string.Respuesta2);
+            aux3=getResources().getString(R.string.Respuesta3);
+            aux4=getResources().getString(R.string.Pregunta);
+            respuestasp1[0] = aux1;
+            respuestasp1[1] = aux2;
+            respuestasp1[2] = aux3;
+            Pregunta p2= new Pregunta(1,aux4,respuestasp1,aux2);
+            preguntas.add(p2);
+            aux1=getResources().getString(R.string.Respuesta1);
+            aux2=getResources().getString(R.string.Respuesta2);
+            aux3=getResources().getString(R.string.Respuesta3);
+            aux4=getResources().getString(R.string.Pregunta);
+            respuestasp1[0] = aux1;
+            respuestasp1[1] = aux2;
+            respuestasp1[2] = aux3;
+            Pregunta p3= new Pregunta(1,aux4,respuestasp1,aux2);
+            preguntas.add(p3);
+
+            obtenerRespuestas(Seleccion);
+
+            switch (view.getId()){
+                case R.id.radioButtonRespuesta1:
+                    if(isChecked){
+                        Seleccion=getString(R.string.Respuesta1);
+                        if(Seleccion.equals(p1.getRespuestaCorrecta())){
+                            respuestasIncorrectas++;
+                            Intent irPantalla2 = new Intent(this, Pantalla2.class);
+                            startActivity(irPantalla2);
+                        }
+                    }
+                    break;
+                case R.id.radioButtonRespuesta2:
+                    if(isChecked){
+                        Seleccion=getString(R.string.Respuesta2);
+                        if(Seleccion.equals(p1.getRespuestaCorrecta())){
+                            respuestasIncorrectas++;
+                            Intent irPantalla2 = new Intent(this, Pantalla2.class);
+                            startActivity(irPantalla2);
+                        }
+                    }
+                    break;
+                case R.id.radioButtonRespuesta3:
+                    //if(isChecked){
+                        Seleccion=getString(R.string.Respuesta3);
+                        if(Seleccion.equals(p1.getRespuestaCorrecta())){
+                            respuestasCorrectas++;
+                            Intent irPantalla2 = new Intent(this, Pantalla2.class);
+                            startActivity(irPantalla2);
+                        }
+                    //}
+                    break;
+                default:
+                    Toast.makeText(this,"No has selecionado nada >:(",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
-    public void clickOnAnswer(View view) {
+    private void obtenerRespuestas(String seleccion) {
 
+    }
+    public void clickOnAnswer(View view) {
+        boolean isChecked = ((RadioButton) view).isChecked();
         switch (view.getId()){
             case R.id.radioButtonRespuesta1:
                 Toast.makeText(this,"Has seleccionado la primera opcion",Toast.LENGTH_LONG).show();
@@ -52,60 +129,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void send(View view) {
-        Boolean isChecked = ((RadioButton) view).isChecked();
-
-        String Seleccion;
-
-        String aux1=getResources().getString(R.string.Respuesta1);
-        String aux2=getResources().getString(R.string.Respuesta2);
-        String aux3=getResources().getString(R.string.Respuesta3);
-        String aux4=getResources().getString(R.string.Pregunta);
-        String[] respuestasp1 = {aux1,aux2,aux3};
-
-        Pregunta p1= new Pregunta(1,aux4,respuestasp1,aux2);
-        preguntas.add(p1);
-        aux1=getResources().getString(R.string.Respuesta1);
-        aux2=getResources().getString(R.string.Respuesta2);
-        aux3=getResources().getString(R.string.Respuesta3);
-        aux4=getResources().getString(R.string.Pregunta);
-        respuestasp1[0] = aux1;
-        respuestasp1[1] = aux2;
-        respuestasp1[2] = aux3;
-        Pregunta p2= new Pregunta(1,aux4,respuestasp1,aux2);
-        preguntas.add(p2);
-        aux1=getResources().getString(R.string.Respuesta1);
-        aux2=getResources().getString(R.string.Respuesta2);
-        aux3=getResources().getString(R.string.Respuesta3);
-        aux4=getResources().getString(R.string.Pregunta);
-        respuestasp1[0] = aux1;
-        respuestasp1[1] = aux2;
-        respuestasp1[2] = aux3;
-        Pregunta p3= new Pregunta(1,aux4,respuestasp1,aux2);
-        preguntas.add(p3);
 
 
-
-        switch (view.getId()){
-            case R.id.radioButtonRespuesta1:
-                if(isChecked){
-                    Seleccion=getString(R.string.Respuesta1);
-                }
-                break;
-            case R.id.radioButtonRespuesta2:
-                if(isChecked){
-                    Seleccion=getString(R.string.Respuesta2);
-                }
-                break;
-            case R.id.radioButtonRespuesta3:
-                if(isChecked){
-                    Seleccion=getString(R.string.Respuesta3);
-                }
-                break;
-            default:
-                Toast.makeText(this,"No has selecionado nada >:(",Toast.LENGTH_LONG).show();
-        }
-
-
-    }
 }
