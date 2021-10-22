e10cad13033e53bc14cda9d1c4b47e396bdbcd88
Jiaz02 <jsaavedraa@iesch.org>
2021-10-22 17:30:19 +0200

    cops&caps

---
+++ Copscaps/app/src/main/java/com/juan/copscaps/modosdejuego/clasico/Activity_Clasic.java
@@ -0,0 +1,16 @@
+package com.juan.copscaps.modosdejuego.clasico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.juan.copscaps.R;
+
+public class Activity_Clasic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clasic);
+    }
+}
