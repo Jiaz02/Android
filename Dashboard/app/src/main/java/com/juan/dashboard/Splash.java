90d26c14a1d82ca9d76f7ecf9826da3d707a7c8d
Jiaz02 <jsaavedraa@iesch.org>
2021-10-26 16:01:56 +0200

    x

---
+++ Dashboard/app/src/main/java/com/juan/dashboard/Splash.java
@@ -0,0 +1,29 @@
+package com.juan.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.juan.dashboard.activities.Activity_Quizzes;
+import com.juan.dashboard.activities.SplashActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash2);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },4000);
+    }
+}
