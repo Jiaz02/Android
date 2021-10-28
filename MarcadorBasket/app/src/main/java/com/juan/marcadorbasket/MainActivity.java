e8a9e2bc11e053a875f20e2386d324bb7799cf8d
Jiaz02 <jsaavedraa@iesch.org>
2021-10-28 13:57:03 +0200

    el fallo esta en el set text

--- MarcadorBasket/app/src/main/java/com/juan/marcadorbasket/MainActivity.java
+++ MarcadorBasket/app/src/main/java/com/juan/marcadorbasket/MainActivity.java
@@ -2,6 +2,7 @@ package com.juan.marcadorbasket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
@@ -23,13 +24,15 @@ public class MainActivity extends AppCompatActivity {
         ImageButton menos1Visitante = findViewById(R.id.buttonR1Visitante);
         ImageButton mas1Visitante = findViewById(R.id.buttonS1Visitante);
         ImageButton mas1Local = findViewById(R.id.buttonS1Local);
+        ImageButton next = findViewById(R.id.siguiente);
 
 
 
         menos1Local.setOnClickListener( view -> {
-            int puntuacionLocal = Integer.parseInt(puntosLocal.getText().toString());
+            String num1 = puntosLocal.getText().toString();
+            int puntuacionLocal = Integer.parseInt(num1);
             int nLocal = puntuacionLocal;
-            if(puntuacionLocal>=0){
+            if(puntuacionLocal>0){
                 puntosLocal.setText(nLocal);
             } else{
                 Toast.makeText(this, getString(R.string.msg_toast),Toast.LENGTH_LONG).show();
@@ -39,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         menos1Visitante.setOnClickListener( view -> {
             int puntuacionVisitante = Integer.parseInt(puntosVisitante.getText().toString());
             int nVisitante = puntuacionVisitante;
-            if(puntuacionVisitante>=0){
+            if(puntuacionVisitante>0){
                 puntosVisitante.setText(nVisitante);
             } else{
                 Toast.makeText(this, getString(R.string.msg_toast),Toast.LENGTH_LONG).show();
@@ -47,7 +50,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
         mas1Visitante.setOnClickListener( view -> {
-            int puntuacionVisitante = Integer.parseInt(puntosVisitante.getText().toString());
+            String num1 = puntosLocal.getText().toString();
+            int puntuacionVisitante = Integer.parseInt(num1);
             int nVisitante = puntuacionVisitante;
             puntosVisitante.setText(nVisitante);
         });
@@ -56,6 +60,11 @@ public class MainActivity extends AppCompatActivity {
             int puntuacionLocal = Integer.parseInt(puntosLocal.getText().toString());
             int nLocal = puntuacionLocal;
             puntosLocal.setText(nLocal);
+        });
+
+        next.setOnClickListener( view -> {
+            Intent intent = new Intent(MainActivity.this,ResultadoActivity.class);
+            startActivity(intent);
         });
     }
 
