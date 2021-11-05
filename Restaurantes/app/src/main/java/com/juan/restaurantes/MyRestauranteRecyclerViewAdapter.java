30bc53c9b946e1eea75bc5ef1ad3bc15e7329979
Jiaz02 <jsaavedraa@iesch.org>
2021-11-05 16:28:02 +0100

    Restaurantes completada

--- Restaurantes/app/src/main/java/com/juan/restaurantes/MyRestauranteRecyclerViewAdapter.java
+++ Restaurantes/app/src/main/java/com/juan/restaurantes/MyRestauranteRecyclerViewAdapter.java
@@ -2,27 +2,33 @@ package com.juan.restaurantes;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
-import com.juan.restaurantes.placeholder.PlaceholderContent.PlaceholderItem;
+import com.bumptech.glide.Glide;
+import com.juan.restaurantes.model.Restaurante;
 import com.juan.restaurantes.databinding.FragmentRestauranteBinding;
 
 import java.util.List;
 
-/**
- * {@link RecyclerView.Adapter} that can display a {@link PlaceholderItem}.
- * TODO: Replace the implementation with code for your data type.
- */
+
 public class MyRestauranteRecyclerViewAdapter extends RecyclerView.Adapter<MyRestauranteRecyclerViewAdapter.ViewHolder> {
 
-    private final List<PlaceholderItem> mValues;
+    private final List<Restaurante> mValues;
+    // rescatamos el contexto desde el fragmento
+    private Context ccx;
 
-    public MyRestauranteRecyclerViewAdapter(List<PlaceholderItem> items) {
+    public MyRestauranteRecyclerViewAdapter(Context context, List<Restaurante> items) {
         mValues = items;
+        ccx = context;
     }
+
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -31,32 +37,57 @@ public class MyRestauranteRecyclerViewAdapter extends RecyclerView.Adapter<MyRes
 
     }
 
+    //METODO IMPORTANTE PORQUE REALIZA EL DIBUJADO DE LA LISTA COMPLETA
+    // Es lanzado tantas veces como elementos tengamos en la lista
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
+
+        // Rescatamos los datos del elementos que ocupa la posicion "position"
         holder.mItem = mValues.get(position);
-        holder.mIdView.setText(mValues.get(position).id);
-        holder.mContentView.setText(mValues.get(position).content);
+        holder.textViewNombreRestaurante.setText(mValues.get(position).getNombre());
+        holder.textViewDireccionRestaurante.setText(mValues.get(position).getDireccion());
+        holder.ratingBarRestaurante.setRating(mValues.get(position).getValoracion());
+        // implementamos la carga de la imagen que ocupa el imageView
+        Glide.with(ccx).load(mValues.get(position).getUrlFoto()).into(holder.imageViewFotoRestaurante);
+
+
     }
 
+    // Devuelve el valor del tamaño de la lista de restaurantes
     @Override
     public int getItemCount() {
         return mValues.size();
     }
 
+    // Esta clase mapea el diseño de los objetos del Layout donde definimos un elemento de la lista, en nuestro caso nuestro restaurante
+    // coge cada elemento de nuestro layout y los asigna a una variable final del tipo que sea
+    // Esta clase depende en gran medida del diseño que tengamos
+
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public final TextView mIdView;
-        public final TextView mContentView;
-        public PlaceholderItem mItem;
+        //cada componente que tenemos en cada fila
+        public final TextView textViewNombreRestaurante;
+        public final TextView textViewDireccionRestaurante;
+        public final ImageView imageViewFotoRestaurante;
+        public final RatingBar ratingBarRestaurante;
+
+        public Restaurante mItem;
+
+        //dentro de este constructor reciviremos por parametro el layout completo
+        // y a partir de ese elemetno raiz podemos buscar el elemento en cuestion
 
         public ViewHolder(FragmentRestauranteBinding binding) {
             super(binding.getRoot());
-            mIdView = binding.itemNumber;
-            mContentView = binding.content;
+
+            textViewNombreRestaurante=binding.textViewNombre;
+            textViewDireccionRestaurante=binding.textViewDireccion;
+            imageViewFotoRestaurante= binding.imageViewRestaurante;
+            ratingBarRestaurante= binding.ratingBarValoracion;
+
         }
 
         @Override
         public String toString() {
-            return super.toString() + " '" + mContentView.getText() + "'";
+            return super.toString() + " '" + textViewNombreRestaurante.getText() + "'";
         }
     }
 }
--- Restaurantes/app/src/main/java/com/juan/restaurantes/RestauranteFragment.java
+++ Restaurantes/app/src/main/java/com/juan/restaurantes/RestauranteFragment.java
@@ -12,22 +12,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.juan.restaurantes.placeholder.PlaceholderContent;
+import com.juan.restaurantes.model.Restaurante;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A fragment representing a list of Items.
  */
 public class RestauranteFragment extends Fragment {
 
+    //Recomendacion: me declaro la variable fuera para acceder a la variable
+    // fuera del onCreate
+    RecyclerView recyclerView;
+    MyRestauranteRecyclerViewAdapter adapterRestaurantes;
+
+    //creamos una lista de restaurante
+    List<Restaurante> restauranteList;
+
     // TODO: Customize parameter argument names
     private static final String ARG_COLUMN_COUNT = "column-count";
     // TODO: Customize parameters
     private int mColumnCount = 1;
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
+
     public RestauranteFragment() {
     }
 
@@ -53,18 +61,35 @@ public class RestauranteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //Este metodo esta asociado a un fichero xml
         View view = inflater.inflate(R.layout.fragment_restaurante_list, container, false);
 
-        // Set the adapter
+        // Aque comprobamos que el elemento padre (XML) sea una insttancia de RecyclerView
         if (view instanceof RecyclerView) {
             Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
+            recyclerView = (RecyclerView) view;
             if (mColumnCount <= 1) {
+                // Gestor del Recycler por defecto. Situa los elementos uno sobre otro
                 recyclerView.setLayoutManager(new LinearLayoutManager(context));
             } else {
                 recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
             }
-            recyclerView.setAdapter(new MyRestauranteRecyclerViewAdapter(PlaceholderContent.ITEMS));
+            //
+            //
+            //
+            //
+
+            //lista de elementos (Restaurante)
+            restauranteList = new ArrayList<>();
+            //añadimos los restaurantes
+            restauranteList.add(new Restaurante("Ichiraku Ramen","https://f.rpp-noticias.io/2019/04/04/423142_774142.png",4.0f,"La Hoja"));
+            restauranteList.add(new Restaurante("The Drunken Clam","https://spoilertime.com/wp-content/uploads/2017/10/b3.gif",4.5f,"Quahog"));
+            restauranteList.add(new Restaurante("Maclaren's Pub","https://media-cdn.tripadvisor.com/media/photo-s/12/9b/3f/a2/ingresso-e-insegna-del.jpg",4.3f,"West 55th Street"));
+            restauranteList.add(new Restaurante("Moe’s","https://spoilertime.com/wp-content/uploads/2017/10/b9.jpg",3.5f,"Springfield"));
+
+            // Asociamos el adaptador al RecyclerView
+            adapterRestaurantes=new MyRestauranteRecyclerViewAdapter(getActivity(),restauranteList);
+            recyclerView.setAdapter(adapterRestaurantes);
         }
         return view;
     }
