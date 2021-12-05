7c48d09783ec62aa1a416e94f822c765a53d816b
jiaz02 <jsaavedraa@iesch.org>
2021-12-05 21:07:21 +0100

    Mapa terminado

--- Copscaps/app/src/main/java/com/juan/copscaps/MainActivity.java
+++ Copscaps/app/src/main/java/com/juan/copscaps/MainActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
 
+import com.juan.copscaps.extras.mapa.MapsActivity;
 import com.juan.copscaps.modosdejuego.clasico.Activity_Clasic;
 
 public class MainActivity extends AppCompatActivity {
 
     RelativeLayout rellay_clasico;
+    RelativeLayout rellar_mapa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +22,13 @@ public class MainActivity extends AppCompatActivity {
 //Para introducir el mapa, hacer que salga la ubicacion de la rae
 
         rellay_clasico=findViewById(R.id.rellay_clasico);
+        rellar_mapa = findViewById(R.id.rellay_mapa);
         rellay_clasico.setOnClickListener(view -> {
             Intent intent = new Intent(this, Activity_Clasic.class);
+            startActivity(intent);
+        });
+        rellar_mapa.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MapsActivity.class);
             startActivity(intent);
         });
     }
